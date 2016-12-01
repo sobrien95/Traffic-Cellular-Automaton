490b9238011076217e3c81eb6850014932dae769
sobrien95 <stephen.obrien91@yahoo.com>
2016-12-01 14:25:03 +0000

    Updated brake rule
    Added a method to the brake rule to simplify finding the gap.

---
+++ Main.c
@@ -0,0 +1,206 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#define VMAX 3
+#define GMAX 5
+#define SIZE 50
+
+struct Car {
+   int id;
+   int velocity;
+   int position;
+   int nextPosition;
+};
+
+int generation;
+int idCount = 1;
+int v = 0;
+int u = 0;
+struct Car *cells[SIZE] = {NULL};
+
+/*
+** Return the smaller of 2 numbers
+*/
+int min(int a, int b)
+{
+	int c = 0;
+
+	if( a < b )
+	{
+		c = a;;
+	}
+
+	else
+	{
+		c = b;
+	}
+
+	return c;
+}
+
+ /*int * make_array()
+ {
+	 int i;
+	 for (i = 0; i < SIZE; i++) {
+
+		 cells[i] = 0;
+	 }
+
+	 return cells;
+ }*/
+
+/*
+** Increase speed of the car towards VMAX
+*/
+int accelerate_rule(int velocity)
+{
+	v = min(velocity + 1,VMAX);
+
+
+	int random = rand() % 10;
+	if(random > 7)
+    {
+        v = randomisation_rule();
+    }
+
+	return v;
+}
+
+int findGap(int position)
+{
+    int gap = 0;
+    int i = 1;
+    while(i < cells[position]->velocity)
+    {
+        if(cells[position + i] == NULL)
+        {
+            gap++;
+        }
+        else
+        {
+            return gap;
+        }
+        i++;
+    }
+}
+
+/*
+**Slow the car down if it's going to collide with the car in front
+*/
+int brake_rule(int speed, int position)
+{
+    int gap = findGap(position);
+    return min(speed, gap);
+}
+
+/*
+** Introduce an element of randomness to the car's velocity
+*/
+int randomisation_rule()
+{
+    printf("Random Rule\n");
+    int speed = rand() % (VMAX + 1);
+    return speed;
+}
+
+int rules()
+{
+	v = min(u,v);
+}
+
+bool isArrayEmpty()
+{
+    int i;
+    bool isEmpty = true;
+    for(i = 0; i < SIZE; i++)
+    {
+        if (cells[i] != NULL)
+        {
+            isEmpty = false;
+        }
+    }
+    return isEmpty;
+}
+
+struct Car *createCar()
+{
+    printf("creating car %d\n", idCount);
+    struct Car *c = (struct Car*)malloc(sizeof(struct Car));
+    //struct Employee* ret = (struct Employee*)malloc(sizeof(struct Employee));
+    c->id = idCount;
+    c->velocity = 0;
+    c->position = 0;
+    idCount ++;
+    return c;
+}
+
+void moveCars()
+{
+    printf("Moving cars\n");
+    int i;
+    for (i = 0; i < SIZE; i ++)
+    {
+        if(cells[i] != NULL)
+        {
+            //accelerate cars (not actually moving yet)
+            cells[i]->velocity = accelerate_rule(cells[i]->velocity);
+            //brake to avoid collisions
+            cells[i]->velocity = brake_rule(cells[i]->velocity, cells[i]->position);
+            printf("car %d velocity = %d, position = %d\n", cells[i]->id, cells[i]->velocity, cells[i]->position);
+            //set next positions
+            cells[i]->nextPosition = cells[i]->position + cells[i]->velocity;
+            //check if any cars have reached the end of the array
+            if(!(cells[i]->nextPosition >= SIZE))
+            {
+                //move cars ahead
+                cells[cells[i]->nextPosition] = cells[cells[i]->position];
+                cells[i]->position = cells[i]->nextPosition;
+            }
+
+            cells[i] = NULL;
+        }
+    }
+}
+
+int main ()
+{
+    srand(time(NULL));
+
+	//	int nextgen [SIZE];
+
+    bool run = true;
+    while(run == true)
+    {
+        while(generation < GMAX)
+        {
+            generation ++;
+            int r = 1;//rand() % 4;
+            if (r == 1 && cells[0] == NULL)
+            {
+                struct Car *car = createCar();
+                cells[0] = car;
+            }
+            moveCars();
+        }
+        moveCars();
+        if(isArrayEmpty())
+        {
+            run = false;
+        }
+
+        //display positions for each car
+        int i;
+        for(i = 0; i < SIZE; i++)
+        {
+            printf("hi");
+            if(cells[i] != NULL)
+            {
+                printf("Car %d speed: %d, position: %d\n",  (*cells[i]).id, (*cells[i]).velocity, i);
+            }
+        }
+        printf("\n");
+    }
+	return 0;
+}
